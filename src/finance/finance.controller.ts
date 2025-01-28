60e78150b738adb55fc66c6014aebd320845d888
Jesus Gil <devjesusgil@gmail.com>
2025-01-28 14:11:11 -0400

    Update finance.controller.ts

--- src/finance/finance.controller.ts
+++ src/finance/finance.controller.ts
@@ -23,6 +23,22 @@ export class FinanceController {
     return this.financeService.findDolarValue(date);
   }
 
+  @Get('save_dollar')
+  async saveDolarValue(): Promise<string> {
+    try {
+      const dolarValue = await this.financeService.fetchDolarValue();
+      const currencyConversionId = 1; // Replace 1 with the actual currency conversion ID
+      await this.financeService.saveDolarValue(
+        dolarValue,
+        currencyConversionId,
+      );
+
+      return 'Dollar value saved successfully.';
+    } catch (error) {
+      return 'Error saving value';
+    }
+  }
+
   // Method to validate time format
   private isValidDate(date: string): boolean {
     // Regex validation for date format YYYY-MM-DD
